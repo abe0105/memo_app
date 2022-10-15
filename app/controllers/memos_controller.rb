941eeff9cfc5c38435c11416c8673053a76fc74b
abe0105 <abe.seiya0105@gmail.com>
2022-10-15 20:05:43 +0900

    メモ機能

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -4,4 +4,34 @@ class MemosController < ApplicationController
     @memos = Memo.all
   end
 
+  def new
+    @memo = Memo.new
+  end
+
+  def create
+    @memo = Memo.create(memo_params)
+    redirect_to memos_path
+  end
+
+  def edit
+    @memo = Memo.find(params[:id])
+  end
+
+  def update
+    @memo = Memo.find(params[:id])
+    @memo.update(memo_params)
+    redirect_to memos_path
+  end
+
+  def destroy
+    @memo = Memo.find(params[:id])
+    @memo.destroy
+    redirect_to memos_path
+  end
+
+  private
+    def memo_params
+      params.require(:memo).permit(:title)
+    end
 end
+
